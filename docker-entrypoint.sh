4c030e75dad4e794b540949907b92620b698fcde
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2025-02-15 20:59:42 +0000

    build(docker-compose): initial compose services for project
    start up qdrantdb, ollama and ollama set up service to pull models into container

---
+++ docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+# See Ollama documentation for pull model(s): https://github.com/ollama/ollama/blob/v0.5.11/docs/api.md#pull-a-model
+# Single quotes won't interpolate anything, but double quotes will.
+for i in $(echo "$OLLAMA_MODELS" | sed "s/,/ /g")
+do
+    curl http://ollama:11434/api/pull -d "{\"name\": \"$i\"}"
+done
